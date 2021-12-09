1df1ae0b56e8e1fd03369f9ed791093c2a7dab61
Christine Banek <cbanek@gmail.com>
2021-12-08 17:57:03 -0700

    [DM-32751] Basic endpoints to expose data
    I've filtered this a few ways, adding one endpoint that just
    dumps the logfile, one that shows errors, and one that shows
    just the ones that seem to be taking a long time.

--- src/sherlock/nginx_parser.py
+++ src/sherlock/nginx_parser.py
@@ -4,44 +4,69 @@ import re
 
 logging.basicConfig(level=logging.WARNING)
 
-class NginxRequestStatistics:
 
+NGINX_LOG_REGEXP = (
+    r"(?P<remote_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) "
+    r"- "
+    r"- "
+    r"\[(?P<time>\d{2}\/[A-Za-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] "
+    r'"(?P<http_verb>(\w+)) (?P<url>.+) HTTP/(2\.0|1\.1)" '
+    r"(?P<status_code>\d+) "
+    r"(?P<body_bytes_sent>\d+) "
+    r'"(?P<http_referer>.+)" '
+    r'"(?P<user_agent>.+)" '
+    r"(?P<request_length>\d+) "
+    r"(?P<request_time>[\.0-9]+) "
+    r"\[(?P<proxy_upstream_name>.+)\] "
+    r"\[(?P<proxy_alternative_upstream>.*)\] "
+    r"(?P<upstream_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+) "
+    r"(?P<upstream_response_length>\d+) "
+    r"(?P<upstream_response_time>[\.0-9]+) "
+    r"(?P<upstream_status>\d+) "
+    r"(?P<request_id>[0-9a-f]+)"
+)
+
+
+class NginxRequestStatistics:
     def __init__(self, data):
-        self.remote_addr = data['remote_addr']
-        self.time_local = data['time_local']
-        self.http_verb = data['http_verb']
-        self.url = data['url']
-        self.status_code = data['status_code']
-        self.body_bytes_sent = data['body_bytes_sent']
-        self.http_referer = data['http_referer']
-        self.user_agent = data['user_agent']
-        self.request_length = data['request_length']
-        self.request_time = data['request_time']
-        self.proxy_upstream_name = data['proxy_upstream_name']
-        self.proxy_alternative_upstream = data['proxy_alternative_upstream']
-        self.upstream_addr = data['upstream_addr']
-        self.upstream_response_length = data['upstream_response_length']
-        self.upstream_response_time = data['upstream_response_time']
-        self.upstream_status = data['upstream_status']
-        self.request_id = data['request_id']
+        """Initialize all the fields.
+
+        Note: the order of these fields is also the order in the dataframe."""
+        self.time = data["time"]
+        self.service_name = data["proxy_upstream_name"]
+        self.http_verb = data["http_verb"]
+        self.status_code = int(data["status_code"])
+        self.url = data["url"]
+        self.body_bytes_sent = int(data["body_bytes_sent"])
+        self.request_length = int(data["request_length"])
+        self.request_time = float(data["request_time"])
+        self.upstream_addr = data["upstream_addr"]
+        self.upstream_response_length = int(data["upstream_response_length"])
+        self.upstream_response_time = float(data["upstream_response_time"])
+        self.upstream_status = data["upstream_status"]
+        self.request_id = data["request_id"]
+        self.http_referer = data["http_referer"]
+        self.proxy_alternative_upstream = data["proxy_alternative_upstream"]
+        self.remote_addr = data["remote_addr"]
+        self.user_agent = data["user_agent"]
 
     def __repr__(self):
-        return f"Request ID: {self.request_id}\n" \
-               f"Remote address: {self.remote_addr}\n" \
-               f"Start time: {self.time_local}\n" \
-               f"Request URL: {self.url}\n" \
-               f"Request VERB: {self.http_verb}\n" \
-               f"Request status: {self.status_code}\n" \
-               f"User agent: {self.user_agent}\n" \
-               f"Request time: {self.request_time}\n" \
-               f"Service: {self.proxy_upstream_name}\n"
+        return (
+            f"Request ID: {self.request_id}\n"
+            f"Remote address: {self.remote_addr}\n"
+            f"Start time: {self.time}\n"
+            f"Request URL: {self.url}\n"
+            f"Request VERB: {self.http_verb}\n"
+            f"Request status: {self.status_code}\n"
+            f"User agent: {self.user_agent}\n"
+            f"Request time: {self.request_time}\n"
+            f"Service: {self.service_name}\n"
+        )
+
 
 class NginxLogParser:
-
-    pattern = r'(?P<remote_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<time_local>\d{2}\/[A-Za-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] "(?P<http_verb>(\w+)) (?P<url>.+) HTTP/(2\.0|1\.1)" (?P<status_code>\d+) (?P<body_bytes_sent>\d+) "(?P<http_referer>.+)" "(?P<user_agent>.+)" (?P<request_length>\d+) (?P<request_time>[\.0-9]+) \[(?P<proxy_upstream_name>.+)\] \[(?P<proxy_alternative_upstream>.*)\] (?P<upstream_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+) (?P<upstream_response_length>\d+) (?P<upstream_response_time>[\.0-9]+) (?P<upstream_status>\d+) (?P<request_id>[0-9a-f]+)'
-
     def __init__(self):
-        self.parser = re.compile(self.pattern)
+        self.parser = re.compile(NGINX_LOG_REGEXP)
 
     def parse_stdin(self):
         for line in fileinput.input():
@@ -52,6 +77,7 @@ class NginxLogParser:
 
         if not matches:
             logging.warning(f"[{line}] doesn't match pattern")
+            return None
         else:
             stats = NginxRequestStatistics(matches.groupdict())
             logging.debug(stats)
--- src/sherlock/nginx_tailer.py
+++ src/sherlock/nginx_tailer.py
@@ -1,14 +1,19 @@
-import logging
+import asyncio
 
+import structlog
 from kubernetes_asyncio import client, config
 from kubernetes_asyncio.client.api_client import ApiClient
-import structlog
 
 from .nginx_parser import NginxLogParser
 
-NGINX_NAMESPACE = 'ingress-nginx'
+NGINX_NAMESPACE = "ingress-nginx"
 
 logger = structlog.get_logger(__name__)
+
+
+total_stats = []
+
+RESTART_SECONDS = 60
 
 
 async def tail_nginx_log():
@@ -19,10 +24,12 @@ async def tail_nginx_log():
             parser = NginxLogParser()
 
             resp = await v1_api.list_namespaced_pod(NGINX_NAMESPACE)
-            resp = await v1_api.read_namespaced_pod_log(resp.items[0].metadata.name,
-                                                        NGINX_NAMESPACE,
-                                                        follow=True,
-                                                        _preload_content=False)
+            resp = await v1_api.read_namespaced_pod_log(
+                resp.items[0].metadata.name,
+                NGINX_NAMESPACE,
+                follow=True,
+                _preload_content=False,
+            )
 
             while True:
                 line = await resp.content.readline()
@@ -30,8 +37,14 @@ async def tail_nginx_log():
                     logger.error("No line retrieved")
                     break
                 else:
-                    stats = parser.parse(line.decode('utf-8'))
-                    logger.info(stats)
+                    stats = parser.parse(line.decode("utf-8"))
+
+                    if stats:
+                        logger.info(stats)
+                        total_stats.append(stats)
         except Exception as e:
             logger.exception(e)
-            logger.error(f"Caught exception {e}, restarting")
+            logger.error(
+                f"Caught {e}, restarting in {RESTART_SECONDS} seconds"
+            )
+            await asyncio.sleep(RESTART_SECONDS)
